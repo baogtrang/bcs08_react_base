7cdd86d4a3ee61881276c655dd9da23e265655d6
BAO TRANG <baogtrang@gmail.com>
2023-08-31 17:36:04 -0500

    done

--- src/DemoState/DemoState.js
+++ src/DemoState/DemoState.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react'
 
 export default class DemoState extends Component {
+  // initialization of the state for a React class component
   state = {
     number: 1,
+    username: "Alice",
   }
 
+  // this.setState để thay đổi giá trị của state
+  //  a special property in React class components. The state object contains data specific to a component that may change over time. 
+  // The component re-renders whenever the state changes.
+  handleIncrease = () => {
+    this.setState({
+      number: ++this.state.number,
+      // number: this.state.number + 1,
+    });    
+  }
+  handleDecrease = () => {
+    this.setState({
+      number: this.state.number - 1,
+    }, () => {
+    })
+  }
+
+  handleChangeName = (name) => {
+    this.setState ({username: name});
+  }
+
+
+  // event handling: có tham số, không có tham số
   render() {
     return (
       <div className="text-center">
-        <button className="btn btn-danger">-</button>
+        <button onClick={this.handleDecrease} className="btn btn-danger">-</button>
         <span className="mx-5 display-4">{this.state.number}</span>
-        <button className="btn btn-success">+</button>
+        <button onClick={this.handleIncrease} className="btn btn-success">+</button>
+        <h2 className={`display-4 ${this.state.username =="Alice" ? "text-warning" : "text-secondary"}`}>{this.state.username}</h2>
 
+        {/* hàm có tham số, bọc lại bằng arrow function */}
+        <button onClick={() => {
+          this.handleChangeName("Bob");
+        }} className="btn btn-secondary">Change to Bob</button>
+
+        <button onClick={()=> {
+          this.handleChangeName("Alice")
+        }} className="btn btn-warning">Change to Alice</button>
       </div>
     )
   }
